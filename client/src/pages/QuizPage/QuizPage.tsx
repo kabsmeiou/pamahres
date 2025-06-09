d25f8c692f154593b5b9fc5b226e58158c69b483
kabsmeiou <cvincentcabral@gmail.com>
2025-06-09 21:25:13 +0800

    fix: disabled start quiz button if qs are not loaded yet

--- client/src/pages/QuizPage/QuizPage.tsx
+++ client/src/pages/QuizPage/QuizPage.tsx
@@ -241,6 +241,7 @@ const QuizPage = () => {
                         <button
                             onClick={handleStartQuiz}
                             className="px-6 py-2 bg-primary-600 text-white font-medium rounded-lg shadow-sm hover:bg-primary-700 focus:ring-2 focus:ring-primary-500 focus:ring-offset-2 transition-all"
+                            disabled={questionsLoading}
                         >
                             Start Quiz
                         </button>
