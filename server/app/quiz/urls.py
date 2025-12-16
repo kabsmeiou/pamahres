9e1af906b2448eb4fdd95422e490324e574c0546
kabsmeiou <cvincentcabral@gmail.com>
2025-12-16 17:04:40 +0800

    quickfix: duplicate urls

--- server/app/quiz/urls.py
+++ server/app/quiz/urls.py
@@ -8,7 +8,4 @@ urlpatterns = [
     path('<int:quiz_id>/delete/', QuizDeleteView.as_view(), name='quiz-delete'),
     path('<int:quiz_id>/', QuizDetailView.as_view(), name='quiz-detail'),
     path('<int:quiz_id>/check-answers/', CheckQuizAnswerView.as_view(), name='check-quiz-answers'),
-    # Quick create endpoints
-    path('quick-create/', QuickCreateQuizView.as_view(), name='quick-create-quiz'),
-    path('quick-create/<int:quiz_id>/', QuickCreateQuizView.as_view(), name='quick-create-quiz-status'),
 ]
